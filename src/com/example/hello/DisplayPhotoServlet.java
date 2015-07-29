da3c83ebca0c69145410907e3e580d26964b0ce4
Leonardo Cserny <leonardo_cserny@netex.ro>
2015-07-29 15:22:06 +0300

    [chapter02] adds DisplayPhotoServlet

---
+++ src/com/example/hello/DisplayPhotoServlet.java
@@ -0,0 +1,39 @@
+package com.example.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
+
+/**
+ * Created by user on 29.07.2015.
+ */
+@WebServlet(name = "DisplayPhotoServlet", urlPatterns = "/DisplayPhotoServlet")
+public class DisplayPhotoServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        String indexString = req.getParameter("photo");
+        int index = (new Integer(indexString.trim())).intValue();
+
+        resp.setContentType("image/jpeg");
+        OutputStream outputStream = resp.getOutputStream();
+
+        try {
+            ServletContext servletContext = req.getServletContext();
+            PhotoAlbum photoAlbum = PhotoAlbum.getPhotoAlbum(servletContext);
+            byte[] bytes = photoAlbum.getPhotoData(index);
+
+            for (int i = 0; i < bytes.length; i++) {
+                outputStream.write(bytes[i]);
+            }
+        } finally {
+            outputStream.close();
+        }
+    }
+}
