25e2c0ff2e5b92635c1e13c022dbbf35bb61603c
Leonardo Cserny <leonardo_cserny@netex.ro>
2015-07-31 10:23:41 +0300

    [chapter02] adds complete PhotoAlbum class

---
+++ src/com/example/hello/RemovePhotoServlet.java
@@ -0,0 +1,31 @@
+package com.example.hello;
+
+import com.example.hello.chapter02.model.PhotoAlbum;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by user on 31.07.2015.
+ */
+@WebServlet(name = "RemovePhotoServlet", urlPatterns = "/RemovePhotoServlet")
+public class RemovePhotoServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        String indexString = req.getParameter("photo");
+        int index = (new Integer(indexString.trim())).intValue();
+        PhotoAlbum photoAlbum = PhotoAlbum.getPhotoAlbum(req.getServletContext());
+
+        photoAlbum.removePhoto(index);
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("DisplayAlbumServlet");
+        requestDispatcher.forward(req, resp);
+    }
+}
--- src/com/example/hello/chapter02/model/PhotoAlbum.java
+++ src/com/example/hello/chapter02/model/PhotoAlbum.java
@@ -1,9 +1,51 @@
 package com.example.hello.chapter02.model;
 
+import java.util.*;
+import javax.servlet.ServletContext;
+
 /**
  * Created by user on 29.07.2015.
  */
 public class PhotoAlbum
 {
+    public static String ATTRIBUTE_NAME = "Photo_Album";
+    private List<byte[]> photoDataList = new ArrayList<byte[]>();
+    private List<String> names = new ArrayList<String>();
 
+    public static PhotoAlbum getPhotoAlbum(ServletContext servletContext)
+    {
+        if (servletContext.getAttribute(ATTRIBUTE_NAME) == null) {
+            PhotoAlbum photoAlbum = new PhotoAlbum();
+            servletContext.setAttribute(ATTRIBUTE_NAME, photoAlbum);
+        }
+
+        return (PhotoAlbum) servletContext.getAttribute(ATTRIBUTE_NAME);
+    }
+
+    public synchronized void addPhoto(String name, byte[] bytes)
+    {
+        this.photoDataList.add(bytes);
+        this.names.add(name);
+    }
+
+    public synchronized byte[] getPhotoData(int i)
+    {
+        return photoDataList.get(i);
+    }
+
+    public synchronized String getPhotoName(int i)
+    {
+        return names.get(i);
+    }
+
+    public synchronized int getPhotoCount()
+    {
+        return photoDataList.size();
+    }
+
+    public synchronized void removePhoto(int i)
+    {
+        photoDataList.remove(i);
+        names.remove(i);
+    }
 }
